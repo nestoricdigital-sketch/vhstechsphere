8b1e0577d2fecbbb86c088ba829df9cd44f74d6e
nestoricdigital-sketch <nestoricdigital@gmail.com>
2026-02-28 14:50:15 +0530

    bug fixed

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,5 +1,5 @@
 // import { useState } from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import StickyBox from "../StickyBox";
 // import { supabase } from "../../superbase/supabaseClient";
 import Form from "./Form";
